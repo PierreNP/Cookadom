d2e7bb0d39691f727c3b54e58cc47b865a3b43f6
GXcode95 <xavier.grenouillet@gmail.com>
2021-09-08 15:48:08 +0200

    set CRUD views + controller dishes

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -15,31 +15,31 @@ class DishesController < ApplicationController
     @dish = Dish.new(dish_params)
 
     if @dish.save 
-      redirect_to @dish, flash[:succes] = "Nouveau plat ajouté !"
+      redirect_to @dish, success:  "Nouveau plat ajouté !"
     else
-      render :new, flash[:error] = "Erreur, impossible d'ajouter le palt'"
+      render :new, error: "Impossible d'ajouter le plat"
     end
   end
 
   def edit;end
 
   def update
-    if @dish.update
-      redirect_to @dish, flash[:succes] = "Nouveau plat ajouté !"
+    if @dish.update(dish_params)
+      redirect_to @dish, success: "Nouveau plat ajouté !"
     else
-      render :edit, flash[:error] = "Erreur : Impossible de modifier le plat."
+      render :edit, error: "Erreur : Impossible de modifier le plat."
     end
   end
 
   def destroy
     @dish.destroy
-    redirect_to root_path, flash[:succes] = "Plat supprimée"
+    redirect_to root_path, success: "Plat supprimée"
   end
 
   private 
     
   def set_dish
-    @dish = dish.find_by(params[:id])
+    @dish = Dish.find(params[:id])
 
   end
 
