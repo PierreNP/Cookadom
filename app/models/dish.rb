e6a1d31508011a0067db840af45c17f620b48ff4
GXcode95 <xavier.grenouillet@gmail.com>
2021-09-08 10:47:01 +0200

    valdiators

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,8 @@
 class Dish < ApplicationRecord
+   validates :name, presence: true, length: { in: 2..40 }
+   validates :description, presence: true, length: { in: 2..400 }
+   validates :price, presence: true
+   validates :ingredients, presence: true
 
    belongs_to :cook
 
@@ -8,5 +12,5 @@ class Dish < ApplicationRecord
    has_many :users, through: :ratings
    has_many :order_dishes
    has_many :carts, through: :order_dishes
-
+   
 end
