b0f0cf59050c95a74a39e6fa7b0c7246f8efee83
GXcode95 <xavier.grenouillet@gmail.com>
2021-09-08 16:07:14 +0200

    merge reset

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+    
+    def show
+    end
+  
+    def edit
+    end
+  
+    def update
+      current_user.update(user_params)
+  
+      respond_to do |format|
+        format.html {redirect_to user_path(current_user.id)}
+        format.js {}
+      end
+    end
+  
+    def destroy
+      current_user.destroy
+  
+      respond_to do |format|
+        format.html {redirect_to root_path}
+        format.js {}
+      end
+    end
+  
+    private
+  
+    def user_params
+      return params.permit(:first_name, :last_name, :status, :phone, :city_id)
+    end 
+end
