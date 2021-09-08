72692f12b587bd41cfac76a48f293fd025976e44
PierreNP <pierre.noppe@essec.edu>
2021-09-08 14:45:42 +0200

    created Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+    def show
+    end
+  
+    def edit
+      @user = User.new
+    end
+  
+    def update
+      @user = current_user
+      @user.update(user_params)
+      edit_profile_send(@user)
+  
+      respond_to do |format|
+        format.html {redirect_to user_path(@user.slug)}
+        format.js {}
+      end
+    end
+  
+    def destroy
+      @cart.destroy
+      current_user.destroy
+  
+      respond_to do |format|
+        format.html {redirect_to root_path}
+        format.js {}
+      end
+    end
+  
+    private
+  
+    def edit_profile_send(user)
+      UserMailer.edit_profile_email(user).deliver_now
+    end
+  
+    def user_params
+      return params.permit(:first_name, :last_name, :address, :email)
+    end 
+end
