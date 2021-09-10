0887bc460eea68e00c64b4bfc74666d8214e8a87
GXcode95 <xavier.grenouillet@gmail.com>
2021-09-10 11:17:51 +0200

    add flash message into a partial

--- app/controllers/order_dishes_controller.rb
+++ app/controllers/order_dishes_controller.rb
@@ -11,7 +11,7 @@ class OrderDishesController < ApplicationController
       order = OrderDish.new(dish_id: params[:dish_id], cart_id: @cart.id, quantity: 1)
       
       if order.save
-        flash[:sucess] = "Plat ajouté au panier"
+        flash[:success] = "Plat ajouté au panier"
       else
         flash[:error] = "Impossible d'ajouter la plat au panier"  
       end
