89f459478e2d655704d47b2320d77460e0765a4a
GXcode95 <xavier.grenouillet@gmail.com>
2021-09-12 22:06:00 +0200

    add button rate to cart

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,10 +1,17 @@
 class RatingsController < ApplicationController
-  def new
-  end
 
   def create
+    if Rating.find_by(user_id: current_user.id, dish_id: params[:dish_id])
+      flash[:error] = "Désolé vous avez déà noté ce plat"
+    else
+      if Rating.create(user_id: current_user.id, dish_id: params[:dish_id], value: params[:value])
+        flash[:success] = "Merci de votre avis !"
+      else 
+        flash[:error] = "Impossible de noter le plat pour le moment"
+      end
+    end
+    redirect_back(fallback_location: root_path)
   end
 
-  def show
-  end
 end
+
