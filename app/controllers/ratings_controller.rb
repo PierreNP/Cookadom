6b1f201fed2b2f5f42fad4a6994d8932afa66802
GXcode95 <xavier.grenouillet@gmail.com>
2021-09-12 23:00:30 +0200

    Prevent user which didn't eat a dish to rate it, in the controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,13 +1,15 @@
 class RatingsController < ApplicationController
 
   def create
-    if Rating.find_by(user_id: current_user.id, dish_id: params[:dish_id])
-      flash[:error] = "Désolé vous avez déà noté ce plat"
-    else
-      if Rating.create(user_id: current_user.id, dish_id: params[:dish_id], value: params[:value])
-        flash[:success] = "Merci de votre avis !"
-      else 
-        flash[:error] = "Impossible de noter le plat pour le moment"
+    unless current_user.past_dishes.include?(Dish.find_by(dish_id: params[:dish_id]))
+      if Rating.find_by(user_id: current_user.id, dish_id: params[:dish_id])
+        flash[:error] = "Désolé vous avez déà noté ce plat"
+      else
+        if Rating.create(user_id: current_user.id, dish_id: params[:dish_id], value: params[:value])
+          flash[:success] = "Merci de votre avis !"
+        else 
+          flash[:error] = "Impossible de noter le plat pour le moment"
+        end
       end
     end
     redirect_back(fallback_location: root_path)
