4d534651c20dfc7df96d3e9e25aca36fa0ef5178
GXcode95 <xavier.grenouillet@gmail.com>
2021-09-14 09:27:39 +0200

    remove deleted and unvalited cook's dish from index

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,7 +2,11 @@ class DishesController < ApplicationController
 
 
   def index
-    @dishes = Dish.search(params[:search]).where(status: "available")
+    dishes = Dish.search(params[:search]).where(status: "available")
+    @dishes = Array.new
+    dishes.each do |dish|
+      @dishes << dish unless dish.cook.status
+    end
   end
 
   def show
