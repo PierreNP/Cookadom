2f4a4333a82271de210192453aebd7ece08d6852
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-09-15 15:55:14 +0200

    Fixing js issues

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,6 +1,6 @@
 function fadeOutFlash() {
     window.addEventListener("DOMContentLoaded", function() {
-        let flash = document.querySelector('#flash');
+        var flash = document.querySelector('#flash');
         console.log(flash);
         setTimeout(function(){ flash.classList.add('opacity-0')}, 5000)
         setTimeout(function(){ flash.innerHTML=""}, 7000)
