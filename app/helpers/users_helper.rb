9e4d31ea0e59cf69ddb83909cc50596f4a944bd4
GXcode95 <xavier.grenouillet@gmail.com>
2021-09-16 14:54:26 +0200

    fix user_helper unread variable

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
 def notification
     if current_user
+        @unread = 0
         current_user.mailbox.conversations.all.each do |conversation|
-            @unread = 0
             conversation.receipts_for(current_user).each do |receipt|
+                puts "#"*200
+                puts "#"*200
             @unread += 1 unless receipt.is_read?
             end
         end
